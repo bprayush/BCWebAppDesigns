c0f48568b1e06a55e6b48e66de220c30211195f8
bprayush <bprayush@yahoo.com>
2017-12-11 12:14:03 +0545

    Added asset() to bootstrap test

--- resources/views/admin/notification/bootstrapTest.blade.php
+++ resources/views/admin/notification/bootstrapTest.blade.php
@@ -11,14 +11,14 @@
 			<h6>Your schedule for <kbd>December 3rd, Monday</kbd> is ready !</h6>
 			<div class="row">
 				<div class="col-md-8 col-lg-8 col-sm-10 col-xs-10" style="background-color:#fff;box-shadow:0px 0px 5px #b5b5b5; border-radius:1px; margin-top:10px;padding:30px;">
-				<img src="images/kasatlogo.jpg" style="float:left;width:32px;border-radius:50px;height:32px;">
+				<img src="{{ asset('images/kasatlogo.jpg') }}" style="float:left;width:32px;border-radius:50px;height:32px;">
 				<span style="font-weight:bold;float:left;margin-left:8px;margin-top:3px;">Kastamandap School</span><br><br>
-				<span style="float:left;margin-left:40px;"><img src="icons/placeholder.png" style="width:20px;">
+				<span style="float:left;margin-left:40px;"><img src="{{ asset('icons/placeholder.png') }}" style="width:20px;">
 					<span style="margin-left:5px;font-size:14px;font-style:italic;">
 						New Road, Kathmandu
 					</span>
 				</span>
-				<span style="float:right;"><img src="icons/calendar.png" style="width:20px;">
+				<span style="float:right;"><img src="{{ asset('icons/calendar.png') }}" style="width:20px;">
 					<span style="margin-left:5px;float:right;font-size:14px;font-weight:bold;">
 						December 3rd 2017
 					</span>
@@ -32,9 +32,9 @@
 				</div>
 				<br><br><br><br><br><hr>
 				<div style="margin-left:130px;">
-					<img src="icons/tick.png" style="width:16px;float:left;"> 
+					<img src="{{ asset('icons/tick.png') }}" style="width:16px;float:left;"> 
 					<span style="margin-left:3px;font-size:13px;">Kit Ready</span><br>
-					<img src="icons/tick.png" style="width:16px;float:left;"> 
+					<img src="{{ asset('icons/tick.png') }}" style="width:16px;float:left;"> 
 					<span style="margin-left:3px;font-size:13px;">Materials Ready</span>
 				</div>
 				</div>
